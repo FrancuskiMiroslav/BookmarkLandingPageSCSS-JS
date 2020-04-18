787bfade5246447710a8e5eb62be50cebd912961
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-04-18 18:23:41 +0200

    sticky responsive navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,25 @@
 /*! project-name v0.0.1 | (c) 2020 Francuski Miroslav | MIT License | http://link-to-your-git-repo.com */
-$(document).ready((function() {
-  console.log("main.js file loaded");
-}));
+window.onload = function () {
+  // scroll sticky nav
+  const header = document.getElementById("sticky");
 
-window.onload = function() {
-  document.addEventListener(
-    "click",
-    (function(event) {
-      if (!event.target.matches("#click-me")) return;
-      alert("You clicked me!");
-    }),
-    false
-  );
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 0) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+
+  // hamburger button toggle
+  const navBtn = document.getElementById("menu-btn");
+  const logo = document.getElementById("main-logo");
+  const nav = document.getElementById("nav");
+
+  navBtn.addEventListener("click", () => {
+    navBtn.classList.toggle("open");
+    logo.classList.toggle("main-logo");
+    header.classList.toggle("overlay");
+    nav.classList.toggle("showing");
+  });
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,24 @@
-$(document).ready(function() {
-  console.log("main.js file loaded");
-});
+window.onload = function () {
+  // scroll sticky nav
+  const header = document.getElementById("sticky");
 
-window.onload = function() {
-  document.addEventListener(
-    "click",
-    function(event) {
-      if (!event.target.matches("#click-me")) return;
-      alert("You clicked me!");
-    },
-    false
-  );
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 0) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+
+  // hamburger button toggle
+  const navBtn = document.getElementById("menu-btn");
+  const logo = document.getElementById("main-logo");
+  const nav = document.getElementById("nav");
+
+  navBtn.addEventListener("click", () => {
+    navBtn.classList.toggle("open");
+    logo.classList.toggle("main-logo");
+    header.classList.toggle("overlay");
+    nav.classList.toggle("showing");
+  });
 };
